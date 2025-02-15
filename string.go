fba9bc78f28dba90e31d668e0b0aa7b6e28f9db0
Randi Febriadi <randifebriadi@gmail.com>
2025-02-15 20:46:53 +0700

    istirahat tidur

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Randi")
+	fmt.Println("Randi Febriadi")
+	fmt.Println("Randi Febriadi Hebat")
+
+	fmt.Println(len("Randi"))
+	fmt.Println("Randi Febriadi"[0])
+	fmt.Println("Randi Febriadi Hebat"[1])
+}
---
+++ variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string
+
+	// name = "Randi Febriadi"
+	// fmt.Println(name)
+
+	// name = "Randi Hebat"
+	// fmt.Println(name)
+
+	var name = "Randi Febriadi"
+
+	fmt.Println(name)
+
+	name = "Randi Hebat"
+	fmt.Println(name)
+}
